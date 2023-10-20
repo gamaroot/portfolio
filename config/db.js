4e63ccf1e698a94a9b3d0da1c3a63d3654756e1d
gama <gamaroot@gmail.com>
2023-10-20 18:22:24 -0400

    Connecting to Mongo Atlas

--- config/db.js
+++ config/db.js
@@ -1,1 +1,2 @@
-module.exports = {"URI":"mongodb://localhost/portfolio"}
+// module.exports = {"URI":"mongodb://192.168.56.1/portfolio"}
+module.exports = {"URI":"mongodb+srv://gamaroot:yD9EPOdCtrCv10fL@0cluster.uu9e2vb.mongodb.net/portfolio?retryWrites=true&w=majority"}
