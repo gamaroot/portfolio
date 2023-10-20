56a0444538ef6449db0954350beb4343fd67717a
gama <gamaroot@gmail.com>
2023-10-20 18:28:54 -0400

    Adding missing comment headers

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
+/**
+ * Centennial College
+ * Game Programming - COMP229 / Section 004
+ * 
+ * Assignment 2 - Portfolio Site using Express JS
+ * 
+ * Author: Carlos da Gama Rocha
+ * Student ID: 301257092
+ * Date: 20-Oct-2023
+ */
+
 module.exports = {"URI":"mongodb://localhost/portfolio"}
+// module.exports = {"URI":"mongodb+srv://gamaroot:yD9EPOdCtrCv10fL@0cluster.uu9e2vb.mongodb.net/portfolio?retryWrites=true&w=majority"}
--- routes/interests.js
+++ routes/interests.js
@@ -1,3 +1,14 @@
+/**
+ * Centennial College
+ * Game Programming - COMP229 / Section 004
+ * 
+ * Assignment 2 - Portfolio Site using Express JS
+ * 
+ * Author: Carlos da Gama Rocha
+ * Student ID: 301257092
+ * Date: 20-Oct-2023
+ */
+
 let express = require('express');
 let router = express.Router();
 
